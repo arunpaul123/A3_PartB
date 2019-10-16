4c7b4c80a3c33fed60e926bd1a81352ffdc916a3
Arun <Arun@156.59.50.76>
2019-10-16 16:19:03 +1300

    Initial commit

---
+++ A3_PartB/src/main/java/com/IT3720/A3_PartB/Arithmetic.java
@@ -0,0 +1,16 @@
+package com.IT3720.A3_PartB;
+
+public class Arithmetic 
+{
+	private int x, y;
+
+	public Arithmetic(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+}
---
+++ A3_PartB/src/test/java/com/IT3720/A3_PartB/ArithmeticTest.java
@@ -0,0 +1,34 @@
+package com.IT3720.A3_PartB;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArithmeticTest {
+	private Arithmetic testObj;
+	private int a, b, expected, actual;
+	
+	@Before
+	public void setUp() throws Exception {
+		a = 10;
+		b = 25;
+		testObj = new Arithmetic(a, b);
+	}
+
+	@Test
+	public void testAdd() {
+		expected = a+b;
+		actual = testObj.add(a, b);
+		assertEquals(expected, actual);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		testObj = null;
+	}
+
+	
+
+}
